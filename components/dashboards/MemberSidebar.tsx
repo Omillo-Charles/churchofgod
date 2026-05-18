545272581c88ae05c6b81a55f11f0fab20cdeb38
Omillo Fidel Charles <fidelomillo812@gmail.com>
2026-05-18 13:35:08 +0300

    feat: replace Create Post link with Gallery navigation in MemberSidebar

--- components/dashboards/MemberSidebar.tsx
+++ components/dashboards/MemberSidebar.tsx
@@ -59,11 +59,13 @@ const navItems = [
         ),
       },
       {
-        name: "Create Post",
-        href: "/portals/member/posts/create",
+        name: "Gallery",
+        href: "/gallery",
         icon: (
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-            <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
+            <rect width="18" height="18" x="3" y="3" rx="2" ry="2"/>
+            <circle cx="9" cy="9" r="2"/>
+            <path d="m21 15-3.086-3.086a2 2 0 0 0-2.828 0L6 21"/>
           </svg>
         ),
       },
